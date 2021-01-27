429aba16f1db86bf3d5d8381ece5f44649d1e963
TNXOH <dimanmityan@yandex.ru>
2021-01-27 16:15:00 +0300

    4 урок

--- 4_1.py
+++
@@ -1,5 +0,0 @@
-from sys import argv
-script_name,output, rate, prize = argv
-a = (int(output) * int(rate)) + int(prize)
-
-print(a)
--- 4_3.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-    from random import randrange
-    print(randrange(20, 240, 21))
